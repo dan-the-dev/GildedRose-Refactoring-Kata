17e5ad1b5bd41581e3600f7445a43b956038f632
danielescillia <daniele.scillia@gmail.com>
2020-12-29 12:56:45 +0100

    A lot of refactoring done. Lot could still be done but this should be enough to make changes easier. More will be done if needed to add the new feature of Conjured

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,58 +1,71 @@
 package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
+    val AGED_BRIE = "Aged Brie"
+    val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+    val SULFURAS = "Sulfuras, Hand of Ragnaros"
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+        items.map {
+            handleQuality(it)
+            handleSellInDays(it)
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
+            if (it.sellInDaysAreNegative()) {
+                handleNegativeDaysQuality(it)
             }
         }
     }
 
+    private fun handleQuality(it: Item) {
+        if (isSpecialItem(it)) {
+            increaseSpecialItemsQuality(it)
+        } else {
+            it.decreaseQuality()
+        }
+    }
+
+    private fun handleSellInDays(it: Item) {
+        if (!it.nameIsEqualsTo(SULFURAS)) {
+            it.decreaseSellIn()
+        }
+    }
+
+    private fun handleNegativeDaysQuality(item: Item) {
+        if (item.nameIsEqualsTo(AGED_BRIE)) {
+            item.increaseQuality()
+            return
+        }
+        if (item.nameIsEqualsTo(BACKSTAGE_PASS)) {
+            item.qualityToLower()
+            return
+        }
+        if (item.nameIsEqualsTo(SULFURAS)) {
+            return
+        }
+
+        item.decreaseQuality()
+    }
+
+    private fun increaseSpecialItemsQuality(item: Item) {
+        item.increaseQuality()
+
+        if (item.nameIsEqualsTo(BACKSTAGE_PASS)) {
+            handleBackstagePassLatesDaysQualityIncrease(item)
+        }
+    }
+
+    private fun handleBackstagePassLatesDaysQualityIncrease(item: Item) {
+        if (item.sellInDaysAreLowerThan(11)) {
+            item.increaseQuality()
+
+            if (item.sellInDaysAreLowerThan(6)) {
+                item.increaseQuality()
+            }
+        }
+    }
+
+    private fun isSpecialItem(item: Item) =
+            item.nameIsEqualsTo(AGED_BRIE) || item.nameIsEqualsTo(BACKSTAGE_PASS) || item.nameIsEqualsTo(SULFURAS)
 }
+
 
--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -4,4 +4,36 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
+
+    fun nameIsEqualsTo(name: String): Boolean {
+        return this.name === name;
+    }
+
+    fun sellInDaysAreNegative(): Boolean {
+        return this.sellIn < 0;
+    }
+
+    fun sellInDaysAreLowerThan(number: Int): Boolean {
+        return this.sellIn < number;
+    }
+
+    fun increaseQuality() {
+        if (this.quality < 50) {
+            this.quality++;
+        }
+    }
+
+    fun decreaseQuality() {
+        if (this.quality > 0) {
+            this.quality--;
+        }
+    }
+
+    fun qualityToLower() {
+        this.quality = 0
+    }
+
+    fun decreaseSellIn() {
+        this.sellIn--;
+    }
 }
